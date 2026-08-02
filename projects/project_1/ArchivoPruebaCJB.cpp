36463110785b399df5d56d407ba367e5f14dc313
OSCAR JAVIER GONZÁLEZ VILLALBA <oscarjavier.gonzalezvillalba@usal.edu.ar>
2026-08-01 23:57:36 -0300

    Actualizacion 2.0
    ultimos cambios hecho, involucran el nuevo archivo modificado en CLion

---
+++ projects/project_1/ArchivoPruebaCJB.cpp
@@ -0,0 +1,16 @@
+//
+// Created by javig on 01/08/2026.
+// esto es una prueba del workflow nuevo que estoy probando
+//
+
+#include <stdio.h>
+
+int main() {
+    int num=0;
+
+    printf("Hola mundo: ");
+    scanf(" %d", &num);fflush(stdin);
+    printf("El numero es: %d", num);
+
+    return 0;
+}
